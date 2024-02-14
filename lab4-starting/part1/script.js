ab67da2853ab4bf914b5147a806701a8cf6f34ef
Leeloo3 <jadenoshaw@gmail.com>
2024-02-14 18:40:38 -0500

    Found a solution to lab 4 part 1

---
+++ lab4-starting/part1/script.js
@@ -0,0 +1,72 @@
+'use strict';
+
+/**
+ * Returns a string of the count and Bet or Hold
+ * @param {array} cards - an array of cards
+ * @returns {string} - message 
+ */
+function countCards(cards) {
+    let count = 0;
+    
+    for (let i = 0; i < cards.length; i++) {
+        let card = cards[i];
+        if (typeof card === 'number') {
+            if (card >= 2 && card <= 6) {
+                count += 1;
+            } else if (card >= 10 || card === 'A') {
+                count -= 1;
+            }
+        } else if (typeof card === 'string') {
+            if (!isNaN(card) && parseInt(card) >= 2 && parseInt(card) <= 6) {
+                count += 1;
+            } else if (['10', 'J', 'Q', 'K', 'A'].includes(card)) {
+                count -= 1;
+            }
+        }
+    }
+    
+    if (count > 0) {
+        return count + " Bet";
+    } else {
+        return count + " Hold";
+    }
+}
+//uncomment following test code after implementing the function
+console.log(countCards([2, 3, 7, 'K', 'A']));
+console.log(countCards([2, 3, 4, 5, 6]));
+console.log(countCards([7, 8, 9]));
+console.log(countCards([10, 'J', 'Q', 'K', 'A']));
+console.log(countCards([3, 7, 'Q', 8, 'A']));
+console.log(countCards([2, 2, 10]));
+console.log(countCards([2, 9, 'J', 2, 7]));
+console.log(countCards([3, 2, 'A', 10, 'K']));
+
+
+
+
+
+
+
+// let count = 0;
+    
+//     for (const card of cards) {
+//         if (typeof card === 'number') {
+//             if (card >= 2 && card <= 6) {
+//                 count += 1;
+//             } else if (card >= 10 || card === 'A') {
+//                 count -= 1;
+//             }
+//         } else if (typeof card === 'string') {
+//             if (!isNaN(card) && parseInt(card) >= 2 && parseInt(card) <= 6) {
+//                 count += 1;
+//             } else if (['10', 'J', 'Q', 'K', 'A'].includes(card)) {
+//                 count -= 1;
+//             }
+//         }
+//     }
+    
+//     if (count > 0) {
+//         return count + " Bet";
+//     } else {
+//         return count + " Hold";
+//     }
