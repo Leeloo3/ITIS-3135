d39600dc139be878ea8aefcac53ab812d63e4a8e
Leeloo3 <jadenoshaw@gmail.com>
2024-03-20 22:38:06 -0400

    Finished Lab 7

--- lab7/script.js
+++ lab7/script.js
@@ -1,12 +1,27 @@
-
 'use strict';
 
 const root = document.documentElement;
-const buttons = document.querySelectorAll(".accordian-label");
+const buttons = document.querySelectorAll(".accordion-label");
 
 function buttonClick(event){
     const btn = event.target;
     btn.classList.toggle("open");
-    const content = btn.nextElementSibling;
-    
+    const contentElement = btn.nextElementSibling;
+    contentElement.classList.toggle("open");
+
+    root.style.setProperty(
+        '--content-height', 
+        contentElement.scrollHeight + 'px'
+    );
+
+    buttons.forEach(button => {
+        if (button !== btn && button.classList.contains('open')) {
+            button.classList.remove('open');
+            button.nextElementSibling.classList.remove('open');
+        }
+    });
 }
+
+buttons.forEach((button) => {
+    button.addEventListener("click", buttonClick);
+});
