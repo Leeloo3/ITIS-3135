1d9be7af63f0e1a754a3e53960dfafcebcee4c0a
Leeloo3 <jadenoshaw@gmail.com>
2024-04-03 20:31:56 -0400

    finished Lab 8

--- lab8/script.js
+++ lab8/script.js
@@ -10,7 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
         try {
             const response = await fetch('./data/iced.json');
             const data = await response.json();
-            console.log(data);
+            // console.log(data);
+            addDrinks(data);
         } catch (error) {
             console.error('Error fetching iced coffee data:', error);
         }
@@ -20,9 +21,49 @@ document.addEventListener('DOMContentLoaded', () => {
         try {
             const response = await fetch('./data/hot.json');
             const data = await response.json();
-            console.log(data);
+            // console.log(data);
+            addDrinks(data)
         } catch (error) {
             console.error('Error fetching hot coffee data:', error);
         }
     });
 });
+
+function addDrinks(drinks) {
+    const container = document.querySelector('.container');
+    container.innerHTML = '';
+
+    drinks.forEach(drink => {
+        const article = document.createElement('article');
+        article.classList.add('card');
+
+        const img = document.createElement('img');
+        img.src = drink.image;
+        img.alt = drink.title;
+        article.appendChild(img);
+
+        const drinkContent = document.createElement('div');
+        drinkContent.classList.add('drinkContent');
+
+        const title = document.createElement('h3');
+        title.textContent = drink.title;
+        drinkContent.appendChild(title);
+
+        const description = document.createElement('p');
+        description.textContent = `${drink.description} Ingredients include:`;
+        drinkContent.appendChild(description);
+
+        const ingredientsContainer = document.createElement('div');
+        ingredientsContainer.classList.add('ingredients');
+        drink.ingredients.forEach(ingredient => {
+            const ingredientDiv = document.createElement('div');
+            ingredientDiv.classList.add('ingredient');
+            ingredientDiv.textContent = ingredient;
+            ingredientsContainer.appendChild(ingredientDiv);
+        });
+
+        drinkContent.appendChild(ingredientsContainer);
+        article.appendChild(drinkContent);
+        container.appendChild(article);
+    });
+}
